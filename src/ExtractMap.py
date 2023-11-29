f0ac163d1e4e93bd6ffbc8e0306452781475f28e
Roude JEAN MARIE <roude.etu@gmail.com>
2023-11-29 09:55:11 +0100

    Rearrangment

--- src/ExtractMap.py
+++ src/ExtractMap.py
@@ -1,31 +1,51 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Extract a feature as a map froma given dataframe
-Ex: 
-    m=ExtractMap(df,"Density20")
+Extract a feature as a map from a given dataframe
+Exemple: 
+    m = ExtractMap(df,"Density20")
     plt.imshow(m)
-    
+
 Created on Wed Sep 7 11:50:57 2022
 
 @author: paolo pierobon
 """
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-def ExtractMap(df, featName, choose=None):
-    if isinstance(choose, str):
-        df = df[df["FileName"] == choose]
 
-    x=np.int32(np.array((df['X']-df['X'].min())/40))
-    y=np.int32(np.array((df['Y']-df['Y'].min())/40))
-    m=np.zeros([y.max()+1,x.max()+1])
-    feat=np.array(df[featName])
-    npoints=df.shape[0]
+def ExtractMap(data, feature_name, chosen_file=None):
+    """Extract the feature map containing in a matrix
+        the features at the respective position. It is
+        used to show regions intensity.
 
-    for i in range(npoints):
-        m[y[i],x[i]]=feat[i]
+        data: pandas.Dataframe
+            A dataframe formatted such that it contains
+            an X and Y column corresponding to positions
+            where a specfic observation has been made.
 
-    return m
+        chosen_file: str
+            In case if dataframe is formatted such that
+
+        Returns: np.ndarray
+            The matrix containing features values
+            associated to each position.
+
+    """
+    df = data
+    if isinstance(chosen_file, str):
+        df = data[data["FileName"] == chosen_file]
+    #
+    n_points = df.shape[0]
+    x = np.array((df['X']-df['X'].min())/40, dtype=np.int32)
+    y = np.array((df['Y']-df['Y'].min())/40, dtype=np.int32)
+    # Matrix of size max(x) * max(y)
+    matrix = np.zeros([y.max()+1, x.max()+1])
+    # Add features to matrix
+    feature_value = np.array(df[feature_name])
+    for i in range(n_points):
+        matrix[y[i], x[i]]= feature_value[i]
+
+    return matrix
