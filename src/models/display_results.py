bc0595aa3a0e80ffe3474b8ca2d36a4faf56bab2
Roude JEAN MARIE <roude.bioinfo@gmail.com>
2024-03-10 20:41:21 +0100

    moved function

---
+++ src/models/display_results.py
@@ -0,0 +1,81 @@
+
+
+__author__ = "Roude JEAN MARIE"
+__email__ = "roude.bioinfo@gmail.com"
+
+
+def x(x_test=None, y_test=None,
+    colnames=None, ):
+
+    # Best hyperparameters
+    print('Best hyperparameters:', rand_search.best_params_) if verbose else None
+    max_depth = rand_search.best_params_['max_depth']
+    n_estimators = rand_search.best_params_['n_estimators']
+
+    # Model with best parameters
+    rf = RandomForestClassifier(
+        max_depth=max_depth,
+        n_estimators=n_estimators,
+        class_weight=class_weight,
+        random_state=seed
+    )
+    rf.fit(x_train, y_train)
+
+    y_pred = rf.predict(x_test)
+    accuracy = metrics.accuracy_score(y_test, y_pred)
+    print("Test Accuracy:", accuracy) if verbose else None
+
+    # Features importance - MDI
+    start_time = time.time()
+    print("Computing MDI importances") if verbose else None
+    importances = rf.feature_importances_
+    std = np.std([tree.feature_importances_ for tree in rf.estimators_], axis=0)
+    elapsed_time = time.time() - start_time
+
+    print(
+        f"Elapsed time to compute mean decrease impurity (MDI) "
+        f"importance: {elapsed_time:.3f} seconds\n"
+    ) if verbose else None
+
+    # Plot - MDI
+    forest_importances = pd.Series(importances, index=columns)
+
+    fig, ax = plt.subplots()
+    forest_importances.plot.bar(yerr=std, ax=ax)
+    ax.set_title("Feature importances using MDI")
+    ax.set_ylabel("Mean decrease in impurity")
+    fig.tight_layout()
+
+    # Features importance - permutation
+    start_time = time.time()
+    print("Computing permutation importances ") if verbose else None
+    result = inspection.permutation_importance(
+        rf, x_test, y_test, n_repeats=10, random_state=seed, n_jobs=-1
+    )
+    elapsed_time = time.time() - start_time
+    print(
+        f"Elapsed time to compute permutation importance: "
+        f"{elapsed_time:.3f} seconds\n"
+    ) if verbose else None
+
+    # Plot - permutation
+    forest_importances = pd.Series(result.importances_mean, index=columns)
+
+    fig, ax = plt.subplots()
+    forest_importances.plot.bar(yerr=result.importances_std, ax=ax)
+    ax.set_title("Feature importances using permutation method")
+    ax.set_ylabel("Mean accuracy decrease")
+    fig.tight_layout()
+    plt.show()
+
+    to_return = {
+        "model": rf,
+        "column": columns,
+        "importance_mdi": {"mean": importances, "std": std},
+        "importance_permutation": {"mean": result.importances_mean, "std": result.importances_std}
+    }
+
+    return to_return
+
+if __name__ == "__main__":
+    pass
