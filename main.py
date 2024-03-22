1cd60f21da1801868549679ce7fa959eeed1a1e8
Roude JEAN MARIE <roude.bioinfo@gmail.com>
2024-03-22 09:30:38 +0100

    Loader & Split (x, y)

--- main.py
+++ main.py
@@ -1,15 +1,58 @@
+import os
+
+import src.utils.constantes as cst
+import src.processing.dataloader as load
+import src.models.models as models
+
 SEED = 42
 
-OUTDIR = constantes.OUTPUT_DIR
-FILEPATH = ""
+MAIN_DIR = "./"
+OUTPUT_DIR = os.path.join(MAIN_DIR, cst.OUTPUT_DIRNAME)
+DATA_DIR = os.path.join(MAIN_DIR, cst.DATA_DIRNAME)
+
+# Attributes for DataLoader
+MASK_CONDITION = [cst.WT]
+MASK_TYPE = [cst.CD3]
+MASK_TUMOR = [cst.IN_TUMOR]  # Only on or outside tumor
+MASK_DENSITY = [cst.IN_FIBER]  # Only in or outside fiber
+
+REMOVE_NONE = True  # True or False
+REPLACE_ABERRANT = -3  # Set to None or actual value
+
+FEATURES = [cst.x_fiber_columns,]
+TARGETS = [load.plus_cmask,]
+TARGETS_COLNAMES = [target_col(return_key=True) for target_col in TARGETS]
+
+# Load data
+loader = load.DataLoader(
+    data_dir=DATA_DIR,
+    mask_condition=MASK_CONDITION,
+    mask_type=MASK_TYPE,
+    mask_tumor=MASK_TUMOR,
+    mask_fiber=MASK_DENSITY,
+    replace_aberrant=REPLACE_ABERRANT,
+    aberrant_columns=cst.aberrant_columns,
+    remove_none=REMOVE_NONE
+)
+
+dataframe = loader.load_data(
+    targets=TARGETS,
+    type=cst.data_type, save=True
+)
+
+# Define X and Y
+for target_column in TARGETS_COLNAMES:
+    for features_column in FEATURES:
+        x, y = models.split_xy(df=dataframe, x_columns=features_column, y_columns=target_column)
+        print(f"{x = }")
+        print(f"{y = }")
+        exit()
+
 CROSS_VAL = 10
 PREDICT_CLASS = ""
 
-TYPE = "cd3"
-CONDITION = ["wt", "ki"]
-SESSION = f"{TYPE}_{'-'.join(CONDITION)}_main" 
-
 if __name__ == "__main__":
+    """
     train_val (colname : train/val)
     cross_val
         matrice de confusion somme
@@ -33,4 +76,5 @@ if __name__ == "__main__":
 
     save_explained
     save_explained_plot
+    """
 
