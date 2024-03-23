52b313137fae29eed47f557a40bf45fd502f601b
Roude JEAN MARIE <roude.bioinfo@gmail.com>
2024-03-23 12:20:08 +0100

    Train & Test

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import src.utils.constantes as cst
 import src.processing.dataloader as load
 import src.models.models as models
 
+
+# Generic attributes
 SEED = 42
+TEST_SIZE = 0.2
 
 MAIN_DIR = "./"
 OUTPUT_DIR = os.path.join(MAIN_DIR, cst.OUTPUT_DIRNAME)
@@ -19,9 +22,14 @@ MASK_DENSITY = [cst.IN_FIBER]  # Only in or outside fiber
 REMOVE_NONE = True  # True or False
 REPLACE_ABERRANT = -3  # Set to None or actual value
 
+# Attributes specifying features, target and sample
 FEATURES = [cst.x_fiber_columns,]
 TARGETS = [load.plus_cmask,]
 TARGETS_COLNAMES = [target_col(return_key=True) for target_col in TARGETS]
+SAMPLE_GROUP = ["FileName",]
+
+# Training regimen
+CV = 2
 
 # Load data
 loader = load.DataLoader(
@@ -37,15 +45,48 @@ loader = load.DataLoader(
 
 dataframe = loader.load_data(
     targets=TARGETS,
-    type=cst.data_type, save=True
+    type=cst.data_type,
+    save=False,
+    force_default=False,
 )
 
 # Define X and Y
 for target_column in TARGETS_COLNAMES:
     for features_column in FEATURES:
-        x, y = models.split_xy(df=dataframe, x_columns=features_column, y_columns=target_column)
-        print(f"{x = }")
-        print(f"{y = }")
+        # Features and Target(s)
+        x, y, groups = models.split_xy(
+            df=dataframe, x_columns=features_column, y_columns=target_column, groups=SAMPLE_GROUP
+        )
+        # Train and Test
+        x_train, x_test, y_train, y_test, groups_train, groups_test = models.split_data(
+            x, y, groups=groups, n_splits=1, test_size=TEST_SIZE, stratify=True, seed=SEED
+        )
+        # Hyperparameters search
+        models.random_forest_search(
+            x=x_train, y=y_train, groups=groups_train,
+            n_split=5, stratify=True, seed=SEED, verbose=0,
+            scoring={
+                    "accuracy": metrics.accuracy_score,
+                    "balanced_accuracy": metrics.balanced_accuracy_score,
+                    "precision": metrics.precision_score,
+                    "recall": metrics.recall_score,
+                    "auc": metrics.roc_auc_score,
+                    "mcc": metrics.matthews_corrcoef,
+                    "f1": metrics.f1_score,
+            },
+            n_iter=5, refit=True, n_jobs=None,
+            class_weight="balanced", random_state=SEED,
+            param_criterion=("entropy",),
+            param_n_estimators=(20, 30, 40, 60),
+            param_max_features=("sqrt,"),
+            param_max_depths=(2, 4, 8, 10, 15, 20),
+            param_min_s_split=(2, 4, 16, 32),
+            param_min_s_leaf=(1, 5),
+            param_bootstrap=(False, True),
+        )
+        print(f"{x_train = }\n{x_test = }")
+        print(f"{y_train = }\n{y_test = }")
+        print(f"{groups_train = }\n{groups_test = }")
         exit()
 
 CROSS_VAL = 10
